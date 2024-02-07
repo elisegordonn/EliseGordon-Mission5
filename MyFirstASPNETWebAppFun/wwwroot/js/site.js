df707758c3a290edd17984ea3afefb5cc155b3e9
elisegordonn <elisegordonn@gmail.com>
2024-02-07 14:17:55 -0700

    Add project files.

---
+++ MyFirstASPNETWebAppFun/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿// Write your JavaScript code.
+
+$(document).ready(function () {
+    $('#btnCalculate').click(function () {
+        var hours = parseFloat($('#hours').val());
+        var hourlyRate = parseFloat($('#hourly-rate').val().substring(1)); // Remove the dollar sign and convert to float
+        var total = hours * hourlyRate;
+
+        if (!isNaN(total) && total > 0) {
+            $('#total').text('$' + total.toFixed(2));
+        } else {
+            alert('Please enter a valid number of hours.');
+        }
+    });
+});
