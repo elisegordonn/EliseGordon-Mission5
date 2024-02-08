9c093ddec7440997263335b0eeff252bd7e6b53f
elisegordonn <elisegordonn@gmail.com>
2024-02-08 13:11:04 -0700

    lkjlk

--- MyFirstASPNETWebAppFun/wwwroot/js/site.js
+++ MyFirstASPNETWebAppFun/wwwroot/js/site.js
@@ -1,11 +1,14 @@
 ﻿// Write your JavaScript code.
 
 $(document).ready(function () {
+/* create a function that starts when the button is clicked*/
     $('#btnCalculate').click(function () {
+/*set the variables with the ids on the page*/
         var hours = parseFloat($('#hours').val());
-        var hourlyRate = parseFloat($('#hourly-rate').val().substring(1)); // Remove the dollar sign and convert to float
+        var hourlyRate = parseFloat($('#hourly-rate').val().substring(1)); 
         var total = hours * hourlyRate;
 
+/*if the total is a valid number, send the output with the total amount*/
         if (!isNaN(total) && total > 0) {
             $('#total').text('$' + total.toFixed(2));
         } else {
